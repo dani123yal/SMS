ac84eb54317b8139943669d961cb4cc279d423d2
dani123yal <daniyal.safdar7@gmail.com>
2019-07-21 15:34:49 +0500

    added chat

---
+++ SMS2/Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMS2.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Create(int id)
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult ParentIndex()
+        {
+            return View();
+        }
+    }
+}
